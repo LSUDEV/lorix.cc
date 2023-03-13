72b0487aaf0fc0c51823d9fb7d467764514d3aa6
LSUDEV <105606754+LSUDEV@users.noreply.github.com>
2023-03-13 17:00:41 -0400

    Update lib.lua

--- utils/lib.lua
+++ utils/lib.lua
@@ -1,5 +1,6 @@
 --[[
     @gs.cc
+    @lorix.cc
 ]]
 -- // Variables
 local ws, uis, rs, hs, cas, plrs, stats = game:GetService("Workspace"), game:GetService("UserInputService"), game:GetService("RunService"), game:GetService("HttpService"), game:GetService("ContextActionService"), game:GetService("Players"), game:GetService("Stats")
